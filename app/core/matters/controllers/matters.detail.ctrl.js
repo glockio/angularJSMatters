9817fc4e62449260d6bd2dd1b2ce5bac267b0025
Greg Laughlin <greg.laughlin81@hotmail.com>
2015-08-09 04:49:45 -0700

    adjust search UI for mobile

--- app/core/matters/controllers/matters.detail.ctrl.js
+++ app/core/matters/controllers/matters.detail.ctrl.js
@@ -1,8 +1,14 @@
 function MatterDetail ($scope, $templateCache, $filter, mattersFactory) {
 
+
+  $scope.shouldShowPlaceholder = function (){
+    console.log("eval")
+    return !!mattersFactory.getFocusMatter()
+  };
+
   $scope.clearFocusMatter = function () {
     mattersFactory.setFocusMatter("");
-  }
+  };
 };
 
 angular.module("app.matters").controller("MatterDetail",  ["$scope", "$templateCache","$filter", "mattersFactory",  MatterDetail]);
